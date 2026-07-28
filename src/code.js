764e5d237e9589223cb1577dcfe8e7b055368cbe
5-cloud835 <justnini98@hotmail.com>
2026-07-28 17:10:24 +0200

    added some content for the page while the AI generates the poem

--- src/code.js
+++ src/code.js
@@ -20,6 +20,10 @@ let prompt=`User instructions: Create a poem with ${userInstructions.value}`;
 let context="You are a poet creating four-line poems in French using the prompt the user has given you. Do not engage with sexual or foul language though. Keep it kid-friendly. Write out the poem using <br/> with each new line. Sign the poem at the bottom with SheCodes AI. ";
 let apiURL=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+ let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${userInstructions.value}</div>`;
+
 axios.get(apiURL).then(displayPoem);
 
 console.log("generating poem");
